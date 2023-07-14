bfc45693479f4367eb06fbe6995ceb2469066c82
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2023-07-13 19:47:12 -0500

    test commit

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@
 // Remember to pay attention to page loading!
 window.addEventListener("load", function () {
   let takeoffBtn = document.getElementById("takeoff");
+  takeoffBtn.addEventListener("click", function () {
+    let response = window.confirm("Ready for Takeoff?");
+    if (response) {
+      document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
+      document.getElementById("shuttleBackground").style.backgroundColor =
+        "blue";
+      changeHeight();
+    }
+  });
 });
 // anElement.addEventListener("eventName", aFunction);
 
